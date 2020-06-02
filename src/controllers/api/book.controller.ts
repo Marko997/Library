3ee4d3070650035f1add3ca98dd69a5c3876eb0d
Marko Simijonovic <marko.simijonovic.16@singimail.rs>
2020-06-02 12:50:41 +0200

    Added delete photo

--- src/controllers/api/book.controller.ts
+++ src/controllers/api/book.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Param, UseInterceptors, UploadedFile, Req } from "@nestjs/common";
+import { Controller, Post, Body, Param, UseInterceptors, UploadedFile, Req, Delete } from "@nestjs/common";
 import { Crud } from "@nestjsx/crud";
 import { BookService } from "../../services/book/book.service";
 import { Book } from "../../entities/book.entity";
@@ -160,5 +160,34 @@ export class BookController{ //dodan u app.module
             })
             .toFile(destinationFilePath);
     }
+    //http://localhost:3000/api/book/1/deletePhoto/30
+    @Delete(':bookId/deletePhoto/:photoId')
+        public async deletePhoto(
+            @Param('bookId') bookId: number,
+            @Param('photoId') photoId: number,)
+            {
+                const photo = await this.photoService.findOne({
+                    bookId: bookId,
+                    photoId: photoId
+                });
+
+                if(!photo){
+                    return new ApiResponse('error',-4004,'Photo not found!');
+                }
+
+                fs.unlinkSync(StorageConfig.photo.destination+photo.imagePath);
+                fs.unlinkSync(StorageConfig.photo.destination+StorageConfig.photo.resize.thumb.directory+photo.imagePath);
+                fs.unlinkSync(StorageConfig.photo.destination+StorageConfig.photo.resize.small.directory+photo.imagePath);
+            
+                const deleteResult = await this.photoService.deleteById(photoId);
+
+                if(deleteResult.affected ===0){
+                    return new ApiResponse('error',-4004,'Photo not found!');
+                }
+
+                return new ApiResponse('ok',0,'One photo deleted!');
+            }
+    
+    
     
 }
